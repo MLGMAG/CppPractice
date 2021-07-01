c8ade1381dfa17750136c6bf2a84deb1d431ced0
Akhmedov, Mahomed <mahomed_akhmedov@epam.com>
2021-07-01 20:07:45 +0300

    [AvrTemp] Add solution and tests

---
+++ src/YellowBelt/Week1/avrTemp/avrTemp.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+int64_t calculateAverage(const vector<int> &data) {
+    int64_t sum = 0;
+    for (const auto &item : data) {
+        sum += item;
+    }
+
+    auto elementCount = static_cast<int64_t> (data.size());
+    return sum / elementCount;
+}
+
+vector<int> getIndexesHigherThanValue(const vector<int> &data, const int64_t &value) {
+    vector<int> result;
+
+    for (int i = 0; i < data.size(); ++i) {
+        if (data[i] > value) {
+            result.push_back(i);
+        }
+    }
+
+    return result;
+}
+
+vector<int> process(const vector<int> &temperatures) {
+    int64_t average = calculateAverage(temperatures);
+    return getIndexesHigherThanValue(temperatures, average);
+}
