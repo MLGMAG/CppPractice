620d22adb7793f1f0a00bad5a9f5644d34038de7
Akhmedov, Mahomed <mahomed_akhmedov@epam.com>
2021-07-03 20:48:34 +0300

    [AvrTemp] Reimplement some logic and write some critical tests

--- test/YellowBelt/Week1/avrTemp/avrTempTest.cpp
+++ test/YellowBelt/Week1/avrTemp/avrTempTest.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 #include <avrTemp.h>
 #include <vector>
+#include <cstdint>
 
 using namespace std;
+
+vector<int64_t> generalTest3InputData();
 
 int main() {
     testing::InitGoogleTest();
@@ -10,7 +13,7 @@ int main() {
 }
 
 TEST(AvrTempTests, generalTest1) {
-    vector<int> inputData = {5, 4, 1, -2, 7};
+    vector<int64_t> inputData = {5, 4, 1, -2, 7};
     vector<int> expected = {0, 1, 4};
 
     vector<int> actual = process(inputData);
@@ -19,10 +22,31 @@ TEST(AvrTempTests, generalTest1) {
 }
 
 TEST(AvrTempTests, generalTest2) {
-    vector<int> inputData = {2, 2, 2, 2, 2};
+    vector<int64_t> inputData = {2, 2, 2, 2, 2};
     vector<int> expected;
 
     vector<int> actual = process(inputData);
 
     EXPECT_EQ(actual, expected);
+}
+
+TEST(AvrTempTests, generalTest3) {
+    vector<int64_t> inputData = generalTest3InputData();
+    vector<int> expected;
+
+    vector<int> actual = process(inputData);
+
+    EXPECT_EQ(actual, expected);
+}
+
+vector<int64_t> generalTest3InputData() {
+    vector<int64_t> data;
+
+    uint64_t elementsCount = 10e6;
+    int64_t element = 10e8;
+    for (uint64_t i = 0; i < elementsCount; ++i) {
+        data.push_back(element);
+    }
+
+    return data;
 }
